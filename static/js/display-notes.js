043ebead53f1ff29e0083230b503bd130b509749
Cynthia Soto <sotocyn@gmail.com>
2016-05-19 16:14:42 -0700

    added id tag to form for note processing and began JS function to display notes

--- static/js/display-notes.js
+++ static/js/display-notes.js
@@ -1,3 +1,12 @@
 "use strict";
 
 // build function that displays/shows a note that was sent through send button
+
+
+function sendNote(evt) {
+    evt.preventDefault();
+
+
+}
+
+$("#send-note").on("submit", sendNote);
